169b878204f5799c036c572c5a5a34a2be0a6ae6
savindha <savi@chaebanicecream.com>
2022-07-12 20:58:16 +0530

    Toggle tabs

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class TabComponent implements OnInit {
 
   constructor() { }
+
+  @Input() tabTitle = ''
+  @Input() active = false
 
   ngOnInit(): void {
   }
