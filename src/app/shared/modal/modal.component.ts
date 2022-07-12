570c1aed79d68148e21571c3cada2e9bb3a4115d
savindha <savi@chaebanicecream.com>
2022-07-12 16:32:35 +0530

    Tab component

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -10,11 +10,12 @@ export class ModalComponent implements OnInit {
 
   @Input() modalID = ''
 
-  constructor(public modal: ModalService) { //this is Dependency Injecting 
+  constructor(public modal: ModalService, public el: ElementRef) { //this is Dependency Injecting 
     // console.log(this.modal.visible)
   }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal() {
