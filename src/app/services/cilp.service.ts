930611b0e62f701753fa94aac6d636b11c663d3f
savindha <savi@chaebanicecream.com>
2022-08-08 20:06:10 +0530

    Getting list of clips

--- src/app/services/cilp.service.ts
+++ src/app/services/cilp.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentReference, QuerySnapshot } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
+import { of } from 'rxjs';
+import { switchMap, map } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +11,29 @@ import IClip from '../models/clip.model';
 export class CilpService {
   public clipsCollection: AngularFirestoreCollection<IClip>
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) {
     this.clipsCollection = db.collection('clips')
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data)
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap(user=> {
+        if(!user) {
+          return of([])
+        }
+        const query = this.clipsCollection.ref.where(
+          'uid', '==', user.uid
+        )
+
+        return query.get()
+      }),
+      map(snapshot=> (snapshot as QuerySnapshot<IClip>).docs)
+    )
   }
 }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
+import { CilpService } from 'src/app/services/cilp.service';
+import IClip from 'src/app/models/clip.model';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
@@ -9,15 +10,27 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 export class ManageComponent implements OnInit {
 
   videoOrder = '1'
+  clips: IClip[] = []
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private clipService: CilpService
   ) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1'
+    })
+    this.clipService.getUserClips().subscribe(docs => {
+      this.clips = []
+      docs.forEach(doc => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data()
+        })
+      })
+
     })
   }
 
@@ -30,7 +43,6 @@ export class ManageComponent implements OnInit {
         sort: value
       }
     })
-
   }
 
 }
