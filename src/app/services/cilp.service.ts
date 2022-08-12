824057b3fee34e15aab311ee16672e39e2aac177
savindha <savi@chaebanicecream.com>
2022-08-12 21:54:15 +0530

    Delete videos

--- src/app/services/cilp.service.ts
+++ src/app/services/cilp.service.ts
@@ -4,6 +4,7 @@ import IClip from '../models/clip.model';
 import { of } from 'rxjs';
 import { switchMap, map } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ export class CilpService {
   public clipsCollection: AngularFirestoreCollection<IClip>
   constructor(
     private db: AngularFirestore,
-    private auth: AngularFireAuth
+    private auth: AngularFireAuth,
+    private storage: AngularFireStorage
   ) {
     this.clipsCollection = db.collection('clips')
   }
@@ -23,8 +25,8 @@ export class CilpService {
 
   getUserClips() {
     return this.auth.user.pipe(
-      switchMap(user=> {
-        if(!user) {
+      switchMap(user => {
+        if (!user) {
           return of([])
         }
         const query = this.clipsCollection.ref.where(
@@ -33,7 +35,21 @@ export class CilpService {
 
         return query.get()
       }),
-      map(snapshot=> (snapshot as QuerySnapshot<IClip>).docs)
+      map(snapshot => (snapshot as QuerySnapshot<IClip>).docs)
     )
+  }
+
+  updateClip(id: string, title: string) {
+    return this.clipsCollection.doc(id).update({
+      title
+    })
+  }
+
+  async deleteClip(clip: IClip) {
+    const clipRef = this.storage.ref(`clips/${clip.fileName}`)
+
+    await clipRef.delete()
+
+    await this.clipsCollection.doc(clip.docID).delete()
   }
 }
--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CilpService } from 'src/app/services/cilp.service';
 
 @Component({
   selector: 'app-edit',
@@ -11,6 +12,11 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EditComponent implements OnInit, OnDestroy, OnChanges {
 
   @Input() activeClip: IClip | null = null
+  inSubmission = false
+  showAlert = false
+  alertColor = 'blue'
+  alertMsg = 'Please wait! Updating clip'
+  @Output() update = new EventEmitter()
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -27,7 +33,8 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
   })
 
   constructor(
-    private modal: ModalService
+    private modal: ModalService,
+    private clipService: CilpService
   ) { }
 
   ngOnInit(): void {
@@ -42,9 +49,40 @@ export class EditComponent implements OnInit, OnDestroy, OnChanges {
     if (!this.activeClip) {
       return
     }
-
+    this.inSubmission = false
+    this.showAlert = false
     this.clipID.setValue(this.activeClip.docID as string)
     this.title.setValue(this.activeClip.title)
+  }
+
+  async submit() {
+
+    if (!this.activeClip) {
+      return
+    }
+
+    this.showAlert = true
+    this.inSubmission = true
+    this.alertMsg = 'Please wait! Updating clip'
+    this.alertColor = 'blue'
+
+    try {
+      await this.clipService.updateClip(this.clipID.value, this.title.value)
+
+    }
+    catch (e) {
+      this.inSubmission = false
+      this.alertMsg = 'Something went wrong. Please try again'
+      this.alertColor = 'red'
+      return
+    }
+
+    this.activeClip.title = this.title.value
+    this.update.emit(this.activeClip)
+
+    this.inSubmission = false
+    this.alertMsg = 'Success'
+    this.alertColor = 'green'
   }
 
 
